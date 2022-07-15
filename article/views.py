973c2f71cb2d0de819777112e755e975de9d76db
Moonmoo <ansaudwn1234@gmail.com>
2022-07-15 16:39:40 +0900

    .

--- article/views.py
+++ article/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 
 
@@ -48,6 +49,8 @@ class ArticleView(APIView):
     # permission_classes = [permissions.AllowAny]
 
     # 모든 게시글 리스팅
+
+    @permission_classes([IsAuthenticated])
     def get(self, request):
         articles = list(ArticleModel.objects.all().order_by("-id"))
         result = ArticleSerializer(articles, many=True).data
